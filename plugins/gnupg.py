e12442bf352c63817958a73edeb112696277f5f2
Karol Będkowski <karol.bedkowski@gmail.com>
2017-07-27 21:31:51 +0200

    update plugins / Kupfer3

--- plugins/gnupg.py
+++ plugins/gnupg.py
@@ -3,7 +3,7 @@ __kupfer_name__ = _("GnuPG Core")
 __kupfer_sources__ = ("PublicKeysSource", )
 __kupfer_actions__ = ("ImportPubKey", "SearchKeys")
 __description__ = _("Core GnuPG functions: access to public keyring, "
-		"import and export keys")
+                    "import and export keys")
 __version__ = "2010-05-29"
 __author__ = "Karol Będkowski <karol.bedkowski@gmail.com>"
 
@@ -17,45 +17,48 @@ from kupfer.obj import contacts
 
 import gnupg_support as support
 
-
 PublicKeysSource = support.PublicKeysSource
 
 
 class ImportPubKey(Action):
-	def __init__(self):
-		Action.__init__(self, _("Import Public Key"))
+    def __init__(self):
+        Action.__init__(self, _("Import Public Key"))
 
-	def activate(self, leaf):
-		p = subprocess.Popen(["gpg", "--import"], stdout=subprocess.PIPE,
-				stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-		stdout, stderr = p.communicate(leaf.object)
-		uiutils.show_notification(_("Import Public Key"), stdout or stderr)
+    def activate(self, leaf):
+        p = subprocess.Popen(
+            ["gpg", "--import"],
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate(leaf.object)
+        uiutils.show_notification(_("Import Public Key"), stdout or stderr)
 
-	def item_types(self):
-		yield TextLeaf
+    def item_types(self):
+        yield TextLeaf
 
-	def valid_for_item(self, leaf):
-		return leaf.object.startswith(support.PGP_HEADER_PUBKEY)
+    def valid_for_item(self, leaf):
+        return leaf.object.startswith(support.PGP_HEADER_PUBKEY)
 
-	def get_description(self):
-		return _("Import key into GnuPG public keyring")
+    def get_description(self):
+        return _("Import key into GnuPG public keyring")
 
 
 class SearchKeys(Action):
-	"""Search keys on default keyserver"""
-	def __init__(self):
-		Action.__init__(self, _("Search Key on KeyServer"))
+    """Search keys on default keyserver"""
 
-	def activate_multiple(self, leaves):
-		cli = ['gpg --search-keys']
-		cli.extend((contacts.email_from_leaf(leaf) for leaf in leaves))
-		utils.launch_commandline(' '.join(cli), 'GnuPG', True)
+    def __init__(self):
+        Action.__init__(self, _("Search Key on KeyServer"))
 
-	def activate(self, leaf):
-		self.activate_multiple((leaf, ))
+    def activate_multiple(self, leaves):
+        cli = ['gpg --search-keys']
+        cli.extend((contacts.email_from_leaf(leaf) for leaf in leaves))
+        utils.launch_commandline(' '.join(cli), 'GnuPG', True)
 
-	def item_types(self):
-		yield contacts.ContactLeaf
+    def activate(self, leaf):
+        self.activate_multiple((leaf, ))
 
-	def valid_for_item(self, leaf):
-		return bool(contacts.email_from_leaf(leaf))
+    def item_types(self):
+        yield contacts.ContactLeaf
+
+    def valid_for_item(self, leaf):
+        return bool(contacts.email_from_leaf(leaf))
--- plugins/google_docs/__init__.py
+++ plugins/google_docs/__init__.py
@@ -15,92 +15,94 @@ from kupfer.obj.special import PleaseConfigureLeaf, InvalidCredentialsLeaf
 from kupfer import plugin_support, pretty
 
 __kupfer_settings__ = plugin_support.PluginSettings(
-		dict(key='userpass', label='', type=plugin_support.UserNamePassword,
-			value="")
-)
+    dict(
+        key='userpass',
+        label='',
+        type=plugin_support.UserNamePassword,
+        value=""))
 
-
-UPDATE_STARTUP_DELAY=5 #sec
-UPDATE_INTERVAL=15*60 # 15 min
+UPDATE_STARTUP_DELAY = 5  #sec
+UPDATE_INTERVAL = 15 * 60  # 15 min
 
 
 class GoogleDocsLink(UrlLeaf):
-	def get_icon_name(self):
-		return "document"
+    def get_icon_name(self):
+        return "document"
 
 
 def is_plugin_configured():
-	upass = __kupfer_settings__['userpass']
-	return bool(upass and upass.username and upass.password)
+    upass = __kupfer_settings__['userpass']
+    return bool(upass and upass.username and upass.password)
 
 
 def get_gclient():
-	if not is_plugin_configured():
-		return None
-	gd_client = gdata.docs.service.DocsService(source='kupfer')
-	upass = __kupfer_settings__['userpass']
-	try:
-		gd_client.ClientLogin(upass.username, upass.password)
-	except (gdata.service.BadAuthentication, gdata.service.CaptchaRequired), err:
-		pretty.print_error(__name__, 'get_gclient', 'authentication error',
-				err)
-		upass.password = None
-		__kupfer_settings__['userpass'] = upass
-		return None
-	else:
-		return gd_client
+    if not is_plugin_configured():
+        return None
+    gd_client = gdata.docs.service.DocsService(source='kupfer')
+    upass = __kupfer_settings__['userpass']
+    try:
+        gd_client.ClientLogin(upass.username, upass.password)
+    except (gdata.service.BadAuthentication,
+            gdata.service.CaptchaRequired) as err:
+        pretty.print_error(__name__, 'get_gclient', 'authentication error',
+                           err)
+        upass.password = None
+        __kupfer_settings__['userpass'] = upass
+        return None
+    else:
+        return gd_client
 
 
 def get_docs():
-	pretty.print_debug(__name__, 'get_docs start')
-	start_time = time.time()
+    pretty.print_debug(__name__, 'get_docs start')
+    start_time = time.time()
 
-	if not is_plugin_configured():
-		yield PleaseConfigureLeaf('google_docs', __kupfer_name__)
-		return
+    if not is_plugin_configured():
+        yield PleaseConfigureLeaf('google_docs', __kupfer_name__)
+        return
 
-	gd_client = get_gclient()
-	if gd_client is None:
-		yield InvalidCredentialsLeaf('google_docs', __kupfer_name__)
-		return
+    gd_client = get_gclient()
+    if gd_client is None:
+        yield InvalidCredentialsLeaf('google_docs', __kupfer_name__)
+        return
 
-	try:
-		for entry in gd_client.GetDocumentListFeed().entry:
-			name = entry.title.text.encode('UTF-8')
-			link = entry.GetAlternateLink().href
-			yield GoogleDocsLink(link, name)
-	except gdata.service.Error, err:
-		pretty.print_error(__name__, 'get_docs', err)
-	pretty.print_debug(__name__, 'get_docs finished',
-			str(time.time()-start_time))
+    try:
+        for entry in gd_client.GetDocumentListFeed().entry:
+            name = entry.title.text.encode('UTF-8')
+            link = entry.GetAlternateLink().href
+            yield GoogleDocsLink(link, name)
+    except gdata.service.Error as err:
+        pretty.print_error(__name__, 'get_docs', err)
+    pretty.print_debug(__name__, 'get_docs finished',
+                       str(time.time() - start_time))
 
 
 class GoogleDocsSource(Source):
-	source_user_reloadable = True
+    source_user_reloadable = True
 
-	def __init__(self, name=_("Google Docs")):
-		super(GoogleDocsSource, self).__init__(name)
+    def __init__(self, name=_("Google Docs")):
+        super(GoogleDocsSource, self).__init__(name)
 
-	def initialize(self):
-		Source.initialize(self)
-		__kupfer_settings__.connect("plugin-setting-changed", self._changed)
+    def initialize(self):
+        Source.initialize(self)
+        __kupfer_settings__.connect("plugin-setting-changed", self._changed)
 
-	def get_items(self):
-		return get_docs() or []
+    def get_items(self):
+        return get_docs() or []
 
-	def provides(self):
-		yield UrlLeaf
-		yield PleaseConfigureLeaf
+    def provides(self):
+        yield UrlLeaf
+        yield PleaseConfigureLeaf
 
-	def should_sort_lexically(self):
-		return True
+    def should_sort_lexically(self):
+        return True
 
-	def get_description(self):
-		return _("Documents from Google")
+    def get_description(self):
+        return _("Documents from Google")
 
-	def get_icon_name(self):
-		return __name__ + "googledocs"
+    def get_icon_name(self):
+        return __name__ + "googledocs"
 
-	def _changed(self, settings, key, value):
-		if key == "userpass":
-			self.mark_for_update()
+    def _changed(self, settings, key, value):
+        if key == "userpass":
+            self.mark_for_update()
